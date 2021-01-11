0f609f670b482e0c38108dfeaaf87de1b03d67c6
Mo <hamada.ashour.93@gmail.com>
2021-01-11 19:38:04 +0100

    added custom user profile model, manager and migration

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# custom user model and custom user manager as django default user model instead of the one provided by django
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
